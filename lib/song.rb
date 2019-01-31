080adf62c072d23eab07382003cbc2fda57a17d1
eselbeus <eselbeus@gmail.com>
2019-01-31 19:31:06 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -6,6 +6,9 @@ class Song
   @@artists = []
   
   def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
     
     @@artists << @artist
     @@genres << @genre
@@ -15,14 +18,14 @@ class Song
     
     @@count
   end
-  def self.genres 
-    genre_count = Hash.new(0)
-    @@genres.each do |genre|
-      genre_count[genre] +=1
-    end
+  # def self.genres 
+  #   genre_count = Hash.new(0)
+  #   @@genres.each do |genre|
+  #     genre_count[genre] +=1
+  #   end
     
-    @@genres = genre_count
-  end
+  #   @@genres = genre_count
+  # end
   # self.artists
     
   # end
