94a3a2a740e60322f51604fdaa1f0e57dd746563
eselbeus <eselbeus@gmail.com>
2019-01-31 19:17:45 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,11 +2,32 @@ class Song
   attr_accessor :name, :artist, :genre
   
   @@count = 0
+  @@genres = []
+  @@artists = []
   
-  def initialize
+  def initialize(name, artist, genre)
     
-    
+    @@artists << @artist
+    @@genres << @genre
     @@count += 1
   end
+  def self.count
+    
+    @@count
+  end
+  def self.genres 
+    
+    
+  end
+  self.artists
+    
+  end
+  self.genre_count
+  
+  end
+  self.artist_count
+    
+  end
+  
   
 end
